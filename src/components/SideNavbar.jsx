7717e477c12af3bb3dbcf2c864914b558aa99af3
Al Samir <shahinur1591@gmail.com>
2024-05-11 22:19:25 +0600

    Ant-desing UI added and bug fixed

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,79 +1,112 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
+import { Drawer } from "antd";
 import DragHandleIcon from "@mui/icons-material/DragHandle";
 import { Link, NavLink } from "react-router-dom";
-import Tooltip from "@mui/material/Tooltip";
 import { AuthContext } from "../Provider/AuthProvider";
+import { Menu, MenuItem } from "@mui/material";
 
 export default function SideNavbar() {
   const [open, setOpen] = React.useState(false);
   const { user, logOut } = React.useContext(AuthContext);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openE = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <div className="flex flex-col gap-5 mt-8">
-        <NavLink
-          to={`/`}
-          className={`mx-6 relative font-semibold hover:text-gold`}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to={`/all-foods`}
-          className={`mx-6 relative font-semibold hover:text-gold`}
-        >
-          All Foods
-        </NavLink>
-        <NavLink
-          to={`/gallery`}
-          className={`mx-6 relative font-semibold hover:text-gold`}
-        >
-          Gallery
-        </NavLink>
-      </div>
-      <div className="ml-5 mt-8">
-        {user ? (
-          <div className="flex flex-col w-32 gap-4">
-            <Tooltip title={user.displayName}>
-              <img
-                className="w-12 rounded-full inline-block mr-4 ml-3"
-                src={user.photoURL}
-                alt=""
-              />
-            </Tooltip>
-            <button
-              onClick={logOut}
-              className="login hover:bg-gold hover:text-white hover:border-gold transition"
-            >
-              Logout
-            </button>
-          </div>
-        ) : (
-          <Link
-            className="login hover:bg-gold hover:text-white hover:border-gold transition"
-            to={`/login`}
-          >
-            Login
-          </Link>
-        )}
-      </div>
-    </Box>
-  );
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div>
-      <Button onClick={toggleDrawer(true)}>
-        <DragHandleIcon className="text-gold" />
-      </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
+    <>
+      <Drawer
+        placement={"left"}
+        closable={false}
+        onClose={onClose}
+        open={open}
+        key={"left"}
+      >
+        <div className="flex flex-col gap-5 mt-8">
+          <NavLink
+            to={`/`}
+            className={` relative font-semibold hover:text-gold`}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={`/all-foods`}
+            className={` relative font-semibold hover:text-gold`}
+          >
+            All Foods
+          </NavLink>
+          <NavLink
+            to={`/gallery`}
+            className={` relative font-semibold hover:text-gold`}
+          >
+            Gallery
+          </NavLink>
+        </div>
+        <div className="mt-8">
+          {user ? (
+            <div className="flex flex-col gap-7 justify-start w-32">
+              <button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <img className="w-12 rounded-full" src={user.photoURL} alt="" />
+              </button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={openE}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+                className="mt-4"
+              >
+                <MenuItem onClick={handleClose} className="menu-item">
+                  <Link to={`/add-food-items`}>Add Food Items</Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose} className="menu-item">
+                  My account
+                </MenuItem>
+                <MenuItem onClick={handleClose} className="menu-item">
+                  Logout
+                </MenuItem>
+              </Menu>
+              <button
+                onClick={logOut}
+                className="login hover:bg-gold hover:text-white hover:border-gold transition"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link
+              className="login hover:bg-gold hover:text-white hover:border-gold transition"
+              to={`/login`}
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </Drawer>
-    </div>
+      <div>
+        <button onClick={showDrawer}>
+          <DragHandleIcon className="text-gold hover:text-navi-blue hover:scale-125 transition" />
+        </button>
+      </div>
+    </>
   );
 }
