55949f4686ba8e443ad1d8722ec68f36c0a9d7f8
Al Samir <shahinur1591@gmail.com>
2024-05-11 19:26:50 +0600

    dynamic title created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    let currentRoute = location.pathname;
+    if (currentRoute == "/") {
+      currentRoute = "";
+    }
+    const modifiedRoute = currentRoute.replace("/", "|");
+    const capitalizedRoute =
+      modifiedRoute.charAt(0) +
+      " " +
+      modifiedRoute.charAt(1).toUpperCase() +
+      modifiedRoute.slice(2);
+    document.title = `Grand Resturant ${capitalizedRoute}`;
+  }, [location]);
   return (
     <>
       <Navbar />
