4f2101cf8b362547af1dcb419eaa1de3b801e0b5
Al Samir <shahinur1591@gmail.com>
2024-05-15 12:16:45 +0600

    Front-end and Backend is deployed successfully

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -5,7 +5,7 @@ import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase.config";
 
 const secureAxios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://assignment-11-server-indol.vercel.app",
   withCredentials: true,
 });
 
